0ce0e8a33c7134e9a17358d860a437a8b14b1bb2
Harry-Park <airroket@outlook.com>
2025-01-06 22:40:40 +0900

    inputevents_mv branch begins
    inputevents_mv.py
    inputevents_mv.py

---
+++ mipipe/inputevents_mv.py
@@ -0,0 +1,7 @@
+from mipipe.inputevents_mv_engineering import *
+from mipipe.config import Config
+from mipipe.mimic_preprocessor import MIMICPreprocessor
+
+class InputeventsMV(MIMICPreprocessor):
+    def __init__(self):
+        super().__init__()
