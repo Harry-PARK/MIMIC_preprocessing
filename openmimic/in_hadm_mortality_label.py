4d651d86bdd254150e0f54ff4095c8f1a51b1205
JINGYU PARK <airroket@outlook.com>
2025-09-09 22:07:27 +0900

    add label making python scripts mortality in_hospital_mortality arf in_arf

---
+++ openmimic/in_hadm_mortality_label.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+
+
+
+if __name__ == "__main__":
+    ADMISSIONS = pd.read_csv("../mimic3_csv/ADMISSIONS.csv", parse_dates=["ADMITTIME", "DISCHTIME", "DEATHTIME"])
+    ICUSTAYS = pd.read_csv("../mimic3_csv/ICUSTAYS.csv")
+
+    ADMISSIONS = ADMISSIONS[["SUBJECT_ID", "HADM_ID", "ADMITTIME", "DISCHTIME", "DEATHTIME"]]
+    ICUSTAYS = ICUSTAYS[["SUBJECT_ID", "HADM_ID", "ICUSTAY_ID"]]
+
+    ADMISSIONS["ADMITTIME"] = pd.to_datetime(ADMISSIONS["ADMITTIME"]).dt.date
+    ADMISSIONS["DISCHTIME"] = pd.to_datetime(ADMISSIONS["DISCHTIME"]).dt.date
+    ADMISSIONS["DEATHTIME"] = pd.to_datetime(ADMISSIONS["DEATHTIME"]).dt.date
+
+    ADMISSIONS["IN_HOSPITAL_MORTALITY"] = ADMISSIONS["DEATHTIME"] == ADMISSIONS["DISCHTIME"]
+    ihm_hadm_ids = set(ADMISSIONS.loc[ADMISSIONS["IN_HOSPITAL_MORTALITY"] == 1, "HADM_ID"])
+    ICUSTAYS["IN_HOSPITAL_MORTALITY"] = ICUSTAYS["HADM_ID"].isin(ihm_hadm_ids).astype(int)
+    ihm_icustay_ids = set(ICUSTAYS.loc[ICUSTAYS["IN_HOSPITAL_MORTALITY"] == 1, "ICUSTAY_ID"])
+
+    pid = np.load("../Synthetic_EHR_Generation/1_real_data/openmimic_preprocessing/earlyAgg/real_earlyAgg_pids.npy")
+    icustay_ids = pid[:, 1]
+    ihm_df = pd.DataFrame({
+        "ICUSTAY_ID": icustay_ids,
+        "IN_HOSPITAL_MORTALITY": 0
+    })
+
+    ihm_df.loc[ihm_df["ICUSTAY_ID"].isin(ihm_icustay_ids), "IN_HOSPITAL_MORTALITY"] = 1
+
+    ihm_label = ihm_df["IN_HOSPITAL_MORTALITY"].values
+    np.save("real_earlyAgg_in_mortality_label.npy", ihm_label)
--- openmimic/mortality_label.py
+++ openmimic/mortality_label.py
@@ -14,19 +14,19 @@ if __name__ == "__main__":
     ADMISSIONS["DISCHTIME"] = pd.to_datetime(ADMISSIONS["DISCHTIME"]).dt.date
     ADMISSIONS["DEATHTIME"] = pd.to_datetime(ADMISSIONS["DEATHTIME"]).dt.date
 
-    ADMISSIONS["IN_HOSPITAL_MORTALITY"] = ADMISSIONS["DEATHTIME"] == ADMISSIONS["DISCHTIME"]
-    ihm_hadm_ids = set(ADMISSIONS.loc[ADMISSIONS["IN_HOSPITAL_MORTALITY"] == 1, "HADM_ID"])
-    ICUSTAYS["IN_HOSPITAL_MORTALITY"] = ICUSTAYS["HADM_ID"].isin(ihm_hadm_ids).astype(int)
-    ihm_icustay_ids = set(ICUSTAYS.loc[ICUSTAYS["IN_HOSPITAL_MORTALITY"] == 1, "ICUSTAY_ID"])
+    ADMISSIONS["MORTALITY"] = ADMISSIONS["DEATHTIME"].notnull()
+    mortality_hadm_ids = set(ADMISSIONS.loc[ADMISSIONS["MORTALITY"] == 1, "HADM_ID"])
+    ICUSTAYS["MORTALITY"] = ICUSTAYS["HADM_ID"].isin(mortality_hadm_ids).astype(int)
+    mortality_icustay_ids = set(ICUSTAYS.loc[ICUSTAYS["MORTALITY"] == 1, "ICUSTAY_ID"])
 
     pid = np.load("../Synthetic_EHR_Generation/1_real_data/openmimic_preprocessing/earlyAgg/real_earlyAgg_pids.npy")
     icustay_ids = pid[:, 1]
-    ihm_df = pd.DataFrame({
+    mortality_df = pd.DataFrame({
         "ICUSTAY_ID": icustay_ids,
-        "IN_HOSPITAL_MORTALITY": 0
+        "MORTALITY": 0
     })
 
-    ihm_df.loc[ihm_df["ICUSTAY_ID"].isin(ihm_icustay_ids), "IN_HOSPITAL_MORTALITY"] = 1
+    mortality_df.loc[mortality_df["ICUSTAY_ID"].isin(mortality_icustay_ids), "MORTALITY"] = 1
 
-    ihm_label = ihm_df["IN_HOSPITAL_MORTALITY"].values
-    np.save("real_in_mortality_label.npy", ihm_label)
+    mortality_label = mortality_df["MORTALITY"].values
+    np.save("real_earlyAgg_mortality_label.npy", mortality_label)
