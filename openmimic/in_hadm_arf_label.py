86e697c8b5c8f7ae6a88b91c48877d823b8bdaa9
JINGYU PARK <airroket@outlook.com>
2025-09-17 14:17:29 +0900

    update 2025.09.17.

--- openmimic/in_hadm_arf_label.py
+++ openmimic/in_hadm_arf_label.py
@@ -13,16 +13,16 @@ if __name__ == "__main__":
 
     mask = DIAGNOSES_ICD["ICD9_CODE"].str.startswith(arf_codes, na=False)
     arf_HADM_ID = DIAGNOSES_ICD.loc[mask, "HADM_ID"].drop_duplicates()
-    ICUSTAYS["ARF"] = ICUSTAYS["HADM_ID"].isin(set(arf_HADM_ID)).astype(int)
+    ICUSTAYS["IN_ARF"] = ICUSTAYS["HADM_ID"].isin(set(arf_HADM_ID)).astype(int)
 
     pid = np.load("../Synthetic_EHR_Generation/1_real_data/openmimic_preprocessing/earlyAgg/earlyAgg_pids.npy")
     icustay_ids = pid[:, 1]
     arf = pd.DataFrame({
         "ICUSTAY_ID": icustay_ids,
-        "ARF": 0
+        "IN_ARF": 0
     })
-    icustay_id_has_arf = set(ICUSTAYS.loc[ICUSTAYS["ARF"] == 1, "ICUSTAY_ID"])
-    arf.loc[arf["ICUSTAY_ID"].isin(icustay_id_has_arf), "ARF"] = 1
+    icustay_id_has_arf = set(ICUSTAYS.loc[ICUSTAYS["IN_ARF"] == 1, "ICUSTAY_ID"])
+    arf.loc[arf["ICUSTAY_ID"].isin(icustay_id_has_arf), "IN_ARF"] = 1
 
-    arf_label = arf["ARF"].values
+    arf_label = arf["IN_ARF"].values
     np.save("in_arf_label.npy", arf_label)
